0058d607f7ad43e941be4a951a27354cb36f7e49
Arunav Singh <arunavsingh999@gmail.com>
2025-03-03 14:45:46 +0530

    added svg to use as sprites

--- CSS Practice B/src/Components/Card.jsx
+++ CSS Practice B/src/Components/Card.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import proImg from "../assets/img/proImg.jpg";
 
 const Card = () => {
   return (
     <>
       <div className="card">
-        <p>Arunav Singh</p>
+        <img src={proImg} alt="Profile Img" className="card__img" />
+
+        <div className="card__details">
+          <p className="card__name">Arunav Singh</p>
+          <p className="card__occupation">Web Developer</p>
+          <ul className="card__icons">
+            <li className="icon--1">Icon--1</li>
+            <li className="icon--2">Icon--2</li>
+            <li className="icon--3">Icon--3</li>
+            <li className="icon--4">Icon--4</li>
+          </ul>
+        </div>
       </div>
     </>
   );
